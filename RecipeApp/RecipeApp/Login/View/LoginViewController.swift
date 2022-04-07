53cbf7f15dc39017bcc0526a434aefd2b0012370
vitor-rc1 <vitor_rc@outlook.com>
2022-04-06 22:07:15 -0300

    LoginView layout

---
+++ RecipeApp/RecipeApp/Login/View/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  RecipeApp
+//
+//  Created by Vitor Conceicao on 06/04/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
