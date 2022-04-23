80f8bf8a7d8dcc9de9f849a64b41802ca0dc990c
vitor-rc1 <vitor_rc@outlook.com>
2022-04-23 20:34:37 -0300

    Alamofire added to Podfile

---
+++ RecipeApp/RecipeAppTests/Services/FoodServiceTests.swift
@@ -0,0 +1,16 @@
+//
+//  FoodServiceTests.swift
+//  RecipeAppTests
+//
+//  Created by Vitor Conceicao on 23/04/22.
+//
+
+import XCTest
+
+class FoodServiceTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
