6005033272aa93707f44a5e80406b98fba49a642
Vitor Rodrigues <42953452+vitor-rc1@users.noreply.github.com>
2025-12-01 23:14:07 -0300

    refactor: adapt project to tma structure
    * refactor: move project to src folder
    
    * refactor: move assets to resources folder
    
    * feat: add scripts
    
    * refactor: update .gitignore to include project files and clean up unnecessary entries
    
    * refactor: update xcov version to ~> 1.9.0 in Gemfile and Gemfile.lock
    
    * refactor: adapt project structure to TMA guidelines with new project and package files
    
    * refactor: update project structure and dependencies, add Info.plist, and adjust test imports
    
    * refactor: update macOS version for build environment in PR workflow
    
    * refactor: add project setup script and integrate it into PR workflow
    
    * refactor: update project setup command to use setup-ci-project in PR workflow
    
    * refactor: remove Podfile and Podfile.lock as part of project restructuring
    
    * refactor: update fastlane version in Gemfile and Gemfile.lock

---
+++ scripts/local-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export XDG_DATA_HOME="$PWD/.mise/data"
+export XDG_STATE_HOME="$PWD/.mise/state"
+export XDG_CACHE_HOME="$PWD/.mise/cache"
+
+echo "✅ Local setup started."
+
+. "$(dirname "$0")/dependencies/mise.sh"
+. "$(dirname "$0")/dependencies/tuist.sh"
+
+echo "✅ Local setup completed."
