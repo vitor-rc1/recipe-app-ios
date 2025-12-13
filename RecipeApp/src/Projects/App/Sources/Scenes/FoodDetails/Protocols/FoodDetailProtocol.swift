247455969c818afd5548bc33d1c2f4c55eb10165
Vitor Conceicao <vitor_rc@outlook.com>
2025-12-12 23:53:19 -0300

    feat(detail): add food detail protocol

---
+++ RecipeApp/src/Projects/App/Sources/Scenes/FoodDetails/Protocols/FoodDetailProtocol.swift
@@ -0,0 +1,18 @@
+//
+//
+//  Created by Vitor Conceicao.
+//
+//  github.com/vitor-rc1
+//
+//
+
+import Foundation
+
+protocol FoodDetailProtocol: Codable {
+    var id: String { get }
+    var name: String { get }
+    var thumb: String { get }
+    var ingredients: [String] { get }
+    var category: String? { get }
+    var instructions: String? { get }
+}
