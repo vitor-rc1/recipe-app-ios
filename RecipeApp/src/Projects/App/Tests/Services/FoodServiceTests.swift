7baabefd9c52fc9a4324dab458178963a02caa98
Vitor Conceicao <vitor_rc@outlook.com>
2026-01-05 22:20:03 -0300

    test: update timeout for expectation in FoodServiceTests

--- RecipeApp/src/Projects/App/Tests/Services/FoodServiceTests.swift
+++ RecipeApp/src/Projects/App/Tests/Services/FoodServiceTests.swift
@@ -164,7 +164,7 @@ final class FoodServiceTests: XCTestCase {
             }
             expectation.fulfill()
         }
-        self.wait(for: [expectation], timeout: 0.1)
+        self.wait(for: [expectation], timeout: 0.2)
     }
     
     func makeSut<FoodType: Codable>(type: FoodType.Type, api: FoodAPI) -> FoodService<FoodType> {
